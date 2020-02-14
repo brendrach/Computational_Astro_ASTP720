20bd28ceb0abca5111945805a7b10a480cad5a2c
abc <abc@example.com>
2020-02-13 21:02:06 -0500

    Adding fully documented code for problems 1-4 as well as the write-up for the assignment. Problem 5 is a work in progress.

--- Assignment_2/Assignment_2.py
+++ Assignment_2/Assignment_2.py
@@ -7,6 +7,16 @@ import numerical_calculus
 
 
 def v_c(r, r_200, v_200, c):
+    '''
+    Summary: 
+    Calculates the circular velocity at a given radius.
+    Parameters
+    ----------
+    r : float, the radius in kiloparsecs.
+    r_200 : float, virial radius.
+    v_200 : float, circular velocity at r_200.
+    c : dimensionless concentration factor. 
+    '''
     x = r / (v_200.value * u.kpc)
     numer = np.log(1 + c * x) - (c * x)/(1 + c * x)
     denom = x * (np.log(1 + c) - (c)/(1 + c))
@@ -15,20 +25,56 @@ def v_c(r, r_200, v_200, c):
 
 
 def M_enc(r, r_200, v_200, c): 
+    '''
+    Summary: 
+    Calculates the mass interior to a radius in solar masses.
+    Parameters
+    ----------
+    r : float, the radius in kiloparsecs.
+    r_200 : float, virial radius.
+    v_200 : float, circular velocity at r_200.
+    c : dimensionless concentration factor. 
+    '''
     return (r * v_c(r, r_200, v_200, c)**2 / const.G).to(u.solMass)
 
 
 def density(r , r_200 , v_200 , c):
+    '''
+    Summary: 
+    Calculates the density as a function of radius, r.
+    Parameters
+    ----------
+    r : float, the radius in kiloparsecs.
+    r_200 : float, virial radius.
+    v_200 : float, circular velocity at r_200.
+    c : dimensionless concentration factor. 
+    '''
     
     r_s = r_200 / c
     return 1 / ((r / r_s) * (1 + r / r_s) ** 2)
 
 
 def M(r , r_200 , v_200 , c):
+    '''
+    Summary: 
+    Calculates the mass as a function of radius, r.
+    Parameters
+    ----------
+    r : float, the radius in kiloparsecs.
+    r_200 : float, virial radius.
+    v_200 : float, circular velocity at r_200.
+    c : dimensionless concentration factor. 
+    '''
+    
     return 4 * np.pi * r**2 * density(r , r_200 , v_200 , c)
 
 
 def problem_2():
+    '''
+    Summary: 
+    Produces plots for problem 2.
+    '''
+    
     r_200 = 230 * u.kpc
     v_200 = 250 * u.km / (u.s)
     c = 8
@@ -74,24 +120,55 @@ def problem_2():
 problem_2()
 
 
-def freq(transition_start , transition_end):
+def freq(upper , lower):
+    '''
+    Summary: 
+    Computes the frequency of a photon emitted when moving
+    from energy state upper to energy state lower.
+    Parameters
+    ----------
+    upper : int, energy state.
+    lower : int, energy state.
+    '''
+    
+    ## Mimics an in text Equation directly under Eq. 8 in the assignment.
     h = const.h
-    freq = abs(( -13.6 * u.eV * transition_end**(-2) ) - ( -13.6 * u.eV * transition_start**(-2)))/h
+    freq = abs(( -13.6 * u.eV * (upper)**(-2) ) - ( -13.6 * u.eV * (lower)**(-2)))/h
     return freq
 
 
-def J(temp , transition_start , transition_end):
+def J(temp , upper , lower):
+    '''
+    Summary: 
+    Calculates the spherically averaged mean intensity from a photon
+    emitted from energy state upper and falls to energy state lower.
+    Parameters
+    ----------
+    temp : float, the temperature of the gas.
+    upper : int, energy state.
+    lower : int, energy state.
+    '''
+    
     h = const.h
     c = const.c
     k = const.k_B
-    freq_value = freq(transition_start , transition_end)
-    freq_value = freq_value.to(1 / u.s)
     
+    ## Calculate the frequency
+    freq_value = freq(upper , lower).to(1 / u.s)
+    
+    ## Calculate the averaged mean intensity.
+    ## Modelled after Eq. 8 in the assignment.
     temp_const = 2 * h * freq_value**3 * c**(-2)
-    temp_exp = h * freq_value / (k * temp)
+    temp_exp = (h * freq_value / (k * temp))
+    temp_exp = (h * freq_value / (k * temp))
     return temp_const / (np.exp(temp_exp) - 1)
     
 def read_file():
+    '''
+    Summary: 
+    Reads in the file containing the Einstein Coeffs.
+    Returns the upper, lower, and A matrices.
+    '''
     
     file = np.genfromtxt('A_coefficients.dat', delimiter=',')
     
@@ -102,6 +179,8 @@ def read_file():
     A = matrix.Matrix([8,8])
     lower = matrix.Matrix([8,8])
     upper = matrix.Matrix([8,8])
+    
+    ## Loop through the matrix and store the data triangularly.
     k = 0
     for i in range(0, 8):
         for j in range(i, 8):
@@ -112,23 +191,58 @@ def read_file():
             
     return lower.transpose(), upper, A
 
+
 def get_lower(lower, upper):
+    '''
+    Summary: 
+    Returns the value of the lower matrix at a given set of indices.
+    ----------
+    upper : int, energy state.
+    lower : int, energy state.
+    '''
+    
     lower = read_file()[0]
     
     return lower.elems[lower][upper]
 
+
 def get_upper(lower, upper):
+    '''
+    Summary: 
+    Returns the value of the upper matrix at a given set of indices.
+    ----------
+    upper : int, energy state.
+    lower : int, energy state.
+    '''
+    
     upper = read_file()[1]
     
     return upper.elems[lower][upper]
 
+
 def get_Aval(lower, upper):
-    Aval = read_file()[2]
+    '''
+    Summary: 
+    Returns the value of the Einstein coeffs at a given set of indices.
+    ----------
+    upper : int, energy state.
+    lower : int, energy state.
+    '''
     
+    Aval = read_file()[2]
     
     return Aval.elems[lower][upper], Aval.elems[upper][lower]
     
-def calc_B(lower, upper):
+
+def get_Bval(lower, upper):
+    '''
+    Summary: 
+    Computes the value of the Einstein Coeff, B.
+    ----------
+    upper : int, energy state.
+    lower : int, energy state.
+    '''
+    
     h = const.h
     c = const.c
     
@@ -140,90 +254,5 @@ def calc_B(lower, upper):
     a = (c ** 2) / (2 * h * freq(lower , upper) ** 3)
     temp_B = a * A_temp
     return temp_B
-
-
-def get_freq(l , up):
-    '''
-    finds the frequency for a particular line transition of hydrogen
-    takes in two energy levels (integers)
-    returns a frequency
-    '''
-    E = abs(( -13.6 * u.eV / (up ** 2) ) - ( -13.6 * u.eV / (l ** 2) ))
-    return E / const.h
-    
-def densities(T):
-
-    '''
-    This function takes in a temperature T
-    computes all of our number densities
-    returns a Matrix object containing all of the number densities
-    '''
-    
-    ###(3x3)
-    b = np.matrix((0,0,1))
-    A = np.zeros((3,3))
-    
-    A.elements[0][0] = ((calc_B(1 , 2) * J(T , 1 , 2) + calc_B(1 , 3) * J(T , 1 , 3))).value
-    A.elements[0][1] = -1 * (get_Aval[0](2 , 1) + calc_B(2 , 1) * J(T , 2 , 1)).value
-    A.elements[0][2] = -1 * (get_Aval[0](3 , 1) + calc_B(3 , 1) * J(T , 3 , 1)).value
-    A.elements[1][1] = (calc_B(2 , 1) * J(T , 2 , 1) + get_Aval(2 , 1) + calc_B(2 , 3) * J(T , 2 , 3)).value
-    A.elements[1][0] = -1 * (calc_B(1 , 2) * J(T  , 1 , 2)).value
-    A.elements[1][2] = -1 * (calc_B(3 , 2) * J(T , 3  ,2) + calc_Aval(3 , 2)).value
-    '''
-    A.elements[2][0] = -1 * (get_B(1 , 3) * J(T , 1 , 3)).value
-    A.elements[2][1] = -1 * (get_B(2 , 3) * J(T , 2 , 3)).value
-    A.elements[2][2] = (get_B(3 , 1) * J(T , 3 , 1) + get_A(3 , 1) + get_A(3, 2) + get_B(3 , 2) * J(T , 3 , 2)).value
-    '''
-    A.elements[2][0] = 1
-    A.elements[2][1] = 1
-    A.elements[2][2] = 1
-    x = matrix.solve_eq(A , b)
-    return x
-
-
-def nden(T):
-    '''
-    This function takes in a temperature T
-    computes all of our number densities
-    returns a Matrix object containing all of the number densities
-    '''
-    
-    n_levels = 9
-    
-    A = matrix.Matrix((n_levels , n_levels))
-    b = matrix.Matrix((n_levels , 1))
-    b.elements[n_levels - 1][0] = 1
-    
-    for i in range(n_levels):
-        ###These for loops set up the matrix A, which contains many einstein coefficieints
-        ### A contains all of the coefficients for a system of equations for the various number densities
-        
-        for j in range(n_levels):
-            if i == n_levels - 1:
-                A.elements[i][j] = 1
-                continue
-            elm = 0
-            if i == j:
-                for k in range(n_levels):
-                    if k == i: ###No transitions from one state to itself
-                        continue
-                    elm += (get_A(i + 1 , k + 1).value)
-                    elm += (get_B(i + 1 , k + 1) * J(T , i + 1 , k + 1)).value
-                A.elements[i][j] = elm
-                
-            else:
-                #for k in range(n_levels):
-
-                elm = (get_B(j + 1 , i + 1) * J(T , j + 1, i + 1) + get_A(j + 1, i + 1)).value
-                A.elements[i][j] = -1 * elm
-        
-
-    ###Now we solve our matrix problem
-    
-    x = matrix.solve_eq(A , b)
-
-    return x
-
-###Runs code to produce all of our plots
 
 
