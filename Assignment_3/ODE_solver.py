0b1890af7214025504b789c90e47781ad35d05f5
abc <abc@example.com>
2020-02-22 11:17:30 -0500

    All 3 integration methods are validated for simple functions. I need to add commments and likely rewrite aspects of the density and dm_dr/dP_dr functions so they work with the current integration inputs.

---
+++ Assignment_3/ODE_solver.py
@@ -0,0 +1,85 @@
+import astropy.units as u
+import astropy.constants as const
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+
+def calc_density(pressure, stellar_type):
+    
+    type_id = id(stellar_type)
+ 
+    if type_id == id("NS") or type_id == id('ns'):
+        density = (pressure/(5.4*10**9))**(3/5)
+    
+    elif type_id == id('WD') or type_id == id('wd'):
+        density = (pressure/1e13)**(3/5) * 2
+        
+    else: 
+        sys.exit("This function only defines the density of white dwarfs (WD) \
+                 and neutron stars (NS).")
+        
+    return density
+        
+
+def euler_method(func, y0, x, dx):    
+    y = np.zeros(len(x))
+    y[0] = y0
+    
+    for i in range(1, len(x)):
+        y[i] = y[i-1] + dx * func(x[i-1], y[i-1])
+            
+    return y
+        
+def heuns_method(func, y0, x, dx):
+    y = np.zeros(len(x))
+    y[0] = y0
+    
+    for i in range(1, len(x)):
+        func_val = func(x[i-1], y[i-1])
+        y[i] = y[i-1] + dx/2 * (func_val + func(x[i], y[i] + dx*func_val))
+            
+    return y
+        
+
+def rk4_method(func, y0, x, dx):
+    y = np.zeros(len(x))
+    y[0] = y0
+    
+    for i in range(1, len(x)):
+        k1 = dx * func(x[i-1], y[i-1])
+        k2 = dx * func(x[i-1] + dx/2, y[i-1] + k1/2)
+        k3 = dx * func(x[i-1] + dx/2, y[i-1] + k2/2)
+        k4 = dx * func(x[i-1] + dx, y[i-1] + k3)
+        y[i] = y[i-1] + 1/6*(k1 + 2*k2 + 2*k3 + k4)
+       
+    return y
+    
+
+def WD_euler_method(func1, func1_start, func2_start, dx):
+    
+    r = [0]
+    M_enc = [func1_start]
+    P_r = [func2_start]
+    
+    i = 0
+    while P_r[i] > 0.00001:
+        
+        r.append(r[i] + dx)
+        M_enc.append(M_enc[i] + dx * dm_dr(r[i], P_r[i], "WD"))
+        P_r.append()        
+
+    
+    return 
+    
+
+def dm_dr(r, pressure, stellar_type):
+    pi = np.pi 
+    return 4 * pi * r**2 * calc_density(pressure, stellar_type)   
+
+def dP_dr(r, pressure, M_enc, stellar_type):
+    G = const.G.cgs    
+    return (-G * M_enc * calc_density(pressure, stellar_type))/r**2 
+    
+    
