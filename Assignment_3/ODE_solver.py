c519380c76bc77296777c0d4c3e4c18cf7f21e3f
abc <abc@example.com>
2020-02-25 02:23:10 -0500

    Adding fully commented ODE solver with unrelated functions removed. The Jupyter notebook is now fully updated containing solutions to each problem. I have to add comments to problems 4 and 5.

--- Assignment_3/ODE_solver.py
+++ Assignment_3/ODE_solver.py
@@ -3,24 +3,6 @@ import astropy.constants as const
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
-
-
-def calc_density(pressure, stellar_type):
-    
-    type_id = id(stellar_type)
- 
-    if type_id == id("NS") or type_id == id('ns'):
-        density = (pressure/(5.4*10**9))**(3/5)
-    
-    elif type_id == id('WD') or type_id == id('wd'):
-        density = (pressure/1e13)**(3/5) * 2
-        
-    else: 
-        sys.exit("This function only defines the density of white dwarfs (WD) \
-                 and neutron stars (NS).")
-        
-    return density
         
 
 def euler_method(func, y0, x, dx):  
@@ -166,16 +148,5 @@ def rk4_method(func, y0, x, dx):
         y[i, :] = y[i-1, :] + 1/6*(k1 + 2*k2 + 2*k3 + k4)
 
     return y
-    
 
-    
-
-def dm_dr(r, pressure, stellar_type):
-    pi = np.pi 
-    return 4 * pi * r**2 * calc_density(pressure, stellar_type)   
-
-def dP_dr(r, pressure, M_enc, stellar_type):
-    G = const.G.cgs    
-    return (-G * M_enc * calc_density(pressure, stellar_type))/r**2 
-    
     
